d20e0ed3e30d3145e3ef20f90cbec59bbede5faf
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-15 09:14:29 +0700

    declaring variables

---
+++ 3.PHP_Variables/variables_1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP variable</title>
+</head>
+<body>
+<?php
+    $txt = "Hello world!";
+    $x = 5;
+    $y = 10.5;
+
+    echo $txt;
+    echo "<br>";
+    echo $x;
+    echo "<br>";
+    echo $y;
+?>
+</body>
+</html>
