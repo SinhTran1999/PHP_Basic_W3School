0d5568d9a1989f0ba7627654183446d74f6c6366
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-15 19:48:35 +0700

    while loop

--- 13.PHP_Loop/while.php
+++ 13.PHP_Loop/while.php
@@ -10,9 +10,18 @@
         $x = 1;
         
         while($x <= 5) {
+            echo "The number is: $x <br>";
+            $x++;
+        } 
+        echo "<br>";
+    ?>  
+    <?php  
+        $x = 0;
+        
+        while($x <= 100) {
         echo "The number is: $x <br>";
-        $x++;
-    } 
+        $x+=10;
+        }
     ?>  
 </body>
 </html>
