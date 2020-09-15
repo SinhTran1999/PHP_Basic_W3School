da43b9697cde42049e3941bf8231afc37550857f
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-15 21:11:16 +0700

    PHP Regular Expression

--- 17.PHP_RegEx/regex_1.php
+++ 17.PHP_RegEx/regex_1.php
@@ -11,5 +11,22 @@
         $pattern = "/w3schools/i";
         echo preg_match($pattern, $str); 
     ?>
+    <?php
+        $str = "The rain in SPAIN falls mainly on the plains.";
+        $pattern = "/ain/i";
+        echo preg_match_all($pattern, $str);
+    ?>
+    <?php
+        $str = "Visit Microsoft!";
+        $pattern = "/microsoft/i";
+        echo preg_replace($pattern, "W3Schools", $str);
+    ?>
+    <?php
+        $str = "Apples and bananas.";
+        $pattern = "/ba(na){2}/i";
+        echo preg_match($pattern, $str);
+    ?>
+    
+
 </body>
 </html>
