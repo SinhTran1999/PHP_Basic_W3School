eaff3b367e18b09c45331f834219ce19e09d54e7
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-15 11:02:26 +0700

    function define() with with array

---
+++ 9.PHP_Constant/constant_2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Constant</title>
+</head>
+<body>
+    <?php
+         define("cars", [
+                "Alfa Romeo",
+                "BMW",
+                "Toyota"
+         ]);
+         echo cars[0];
+    ?> 
+
+</body>
+</html>
