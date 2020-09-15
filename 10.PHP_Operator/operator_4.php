1d91d56b777388f68f7a91d15924b36bc43e0a81
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-15 11:36:01 +0700

    Increment / Decrement Operators

---
+++ 10.PHP_Operator/operator_4.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Operator</title>
+</head>
+<body>
+    <?php
+        $x = 10;  
+        echo ++$x;
+        echo "<br>";
+    ?>  
+    <?php
+        $x = 10;  
+        echo $x++;
+        echo "<br>";
+    ?>  
+    <?php
+        $x = 10;  
+        echo --$x;
+        echo "<br>";
+    ?>  
+    <?php
+        $x = 10;  
+        echo $x--;
+    ?>  
+</body>
+</html>
