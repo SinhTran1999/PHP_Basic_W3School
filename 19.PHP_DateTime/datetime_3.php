0e275c91c210968bf6e3391a25d371719166f3a2
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-15 21:43:44 +0700

    Get a Time

---
+++ 19.PHP_DateTime/datetime_3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Date and Time</title>
+</head>
+<body>
+    <?php
+    echo "The time is " . date("h:i:sa");
+    ?>
+</body>
+</html>
