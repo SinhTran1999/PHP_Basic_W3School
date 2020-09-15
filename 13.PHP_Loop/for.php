5a5d85e436f434627f4b2b4ccc672fcfdd9f7f48
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-15 19:54:12 +0700

    for loop

---
+++ 13.PHP_Loop/for.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Loop</title>
+</head>
+<body>
+    <?php  
+        for ($x = 0; $x <= 10; $x++) {
+        echo "The number is: $x <br>";
+        }
+        echo "<br>";
+    ?> 
+    <?php  
+        for ($x = 0; $x <= 100; $x+=10) {
+        echo "The number is: $x <br>";
+        }
+    ?>  
+</body>
+</html>
