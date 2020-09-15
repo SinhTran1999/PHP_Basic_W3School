f1913896f5584d5f644d38c6de47733ee0e990a1
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-15 23:34:27 +0700

    remove all global session variables and destroy the session

---
+++ 26.PHP_Sessions/session_5.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Session</title>
+</head>
+<body>
+    <?php
+        // remove all session variables
+        session_unset();
+
+        // destroy the session
+        session_destroy();
+
+        echo "All session variables are now removed, and the session is destroyed."
+    ?>
+</body>
+</html>
