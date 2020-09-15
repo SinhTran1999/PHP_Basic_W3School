1fa249b4e5324ab88405e2bcd0b14275479a08cb
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-16 00:07:54 +0700

    Remove Characters With ASCII Value > 127

---
+++ 28.PHP_FilterAdvance/filter_advance_4.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Filter Advanced</title>
+</head>
+<body>
+    <?php
+        // Variable to check
+        $str = "<h1>Hello WorldÆØÅ!</h1>";
+
+        // Remove HTML tags and all characters with ASCII value > 127
+        $newstr = filter_var($str, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+        echo $newstr;
+    ?>
+</body>
+</html>
