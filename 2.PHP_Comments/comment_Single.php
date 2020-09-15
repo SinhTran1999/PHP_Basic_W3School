0c1957b2f897ee0e4ba84db3ad2c0279ecf32040
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-15 09:00:26 +0700

    single-line comments

---
+++ 2.PHP_Comments/comment_Single.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comment Single-line </title>
+</head>
+<body>
+<?php
+// This is a single-line comment
+
+# This is also a single-line comment
+?>
+</body>
+</html>
