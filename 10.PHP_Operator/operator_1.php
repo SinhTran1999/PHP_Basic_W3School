d792b355acb1815533401441d051a242c59f3e16
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-15 11:12:56 +0700

    Arithmetic Operators

---
+++ 10.PHP_Operator/operator_1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Operator</title>
+</head>
+<body>
+    <?php
+        $x = 10;  
+        $y = 6;
+
+        echo $x + $y;
+        echo $x - $y;
+        echo $x * $y;
+        echo $x / $y;
+        echo $x % $y;
+        echo $x ** $y;
+    ?> 
+</body>
+</html>
