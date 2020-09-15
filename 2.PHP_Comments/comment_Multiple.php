6336816a32a74721f5bb5ee13a4bbb97e92241d1
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-15 09:02:21 +0700

    multiple-line comments

---
+++ 2.PHP_Comments/comment_Multiple.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comment multiple-line</title>
+</head>
+<body>
+<?php
+/*
+    Comment multiple-line
+lines
+*/
+?>
+</body>
+</html>
