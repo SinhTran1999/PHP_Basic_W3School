29c3c71d7b89d6b49d2d4aa0a38bbfd119946f2c
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-16 00:06:24 +0700

    Validate URL - Must Contain QueryString

---
+++ 28.PHP_FilterAdvance/filter_advance_3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Filter Advance</title>
+</head>
+<body>
+    <?php
+        // Variable to check
+        $url = "https://www.w3schools.com";
+
+        // Validate url
+        if (!filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_QUERY_REQUIRED) === false) {
+            echo("$url is a valid URL with a query string");
+        } else {
+            echo("$url is not a valid URL with a query string");
+        }
+    ?>
+</body>
+</html>
