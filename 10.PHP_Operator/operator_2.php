ac95ff8bf77a4b0c09f353f0028ccfe54b7b180b
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-15 11:17:46 +0700

    Assignment Operators

---
+++ 10.PHP_Operator/operator_2.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Operator</title>
+</head>
+<body>
+    <?php
+        $x = 10;  
+        echo $x;
+        echo "<br>";
+    ?>  
+    <?php
+        $x = 20;  
+        $x += 100;
+
+        echo $x;
+        echo "<br>";
+    ?>  
+    <?php
+        $x = 50;
+        $x -= 30;
+
+        echo $x;
+        echo "<br>";
+    ?>  
+    <?php
+        $x = 10;  
+        $y = 6;
+
+        echo $x * $y;
+        echo "<br>";
+    ?>  
+    <?php
+        $x = 10;
+        $x /= 5;
+
+        echo $x;
+        echo "<br>";
+    ?>  
+    <?php
+        $x = 15;
+        $x %= 4;
+
+        echo $x;
+    ?>  
+
+</body>
+</html>
