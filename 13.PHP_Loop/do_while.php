8020f4b76d1c0afa8f3628419fd8638fb3fb78d7
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-15 19:51:48 +0700

    do while

---
+++ 13.PHP_Loop/do_while.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Loop</title>
+</head>
+<body>
+    <?php 
+        $x = 1;
+
+        do {
+        echo "The number is: $x <br>";
+        $x++;
+        } while ($x <= 5);
+        echo "<br>";
+    ?> 
+    <?php 
+        $x = 6;
+
+        do {
+        echo "The number is: $x <br>";
+        $x++;
+        } while ($x <= 5);
+    ?>
+</body>
+</html>
