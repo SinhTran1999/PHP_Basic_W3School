166d4dc77b18563b0baa07de86fde5b97b9db38d
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-15 11:31:45 +0700

    Comparison Operators

--- 10.PHP_Operator/operator_3.php
+++ 10.PHP_Operator/operator_3.php
@@ -68,24 +68,6 @@
 
         var_dump($x <= $y); // returns true because $x is less than or equal to $y
         echo "<br>";
-    ?>  
-    <?php
-        $x = 5;  
-        $y = 10;
-
-        echo ($x <=> $y); // returns -1 because $x is less than $y
-        echo "<br>";
-
-        $x = 10;  
-        $y = 10;
-
-        echo ($x <=> $y); // returns 0 because values are equal
-        echo "<br>";
-
-        $x = 15;  
-        $y = 10;
-
-        echo ($x <=> $y); // returns +1 because $x is greater than $y
-    ?>  
+    ?>    
 </body>
 </html>
