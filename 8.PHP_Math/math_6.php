2d88b73bbb6354aa2de02d7da899a0167e03ea92
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-15 10:52:58 +0700

    function rand()

---
+++ 8.PHP_Math/math_6.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Math</title>
+</head>
+<body>      
+    <?php
+        echo(rand());
+        echo "<br>";
+        echo(rand(10, 100));
+    ?>
+</body>
+</html>
