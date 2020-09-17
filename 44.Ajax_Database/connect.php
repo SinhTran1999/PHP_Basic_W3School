d06c7a5653b90e58711d1132c6768753b4e46df6
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-17 09:26:26 +0700

    create data in table DataUser MySQL

--- 44.Ajax_Database/connect.php
+++ 44.Ajax_Database/connect.php
@@ -2,18 +2,47 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
+    $dbname = "dataConnect";
     // Create connection
-    $conn = new mysqli($servername, $username, $password);
+    $conn = new mysqli($servername, $username, $password, $dbname);
     // Check connection
     if($conn->connect_error){
         die("Connection failed: ".$conn->connect_error);
     }
-    // Create database
-    $sql = "CREATE DATABASE dataConnect";
-    if($conn->query($sql) === TRUE){
-        echo "Database created successfully";
-    }else{
-        echo "Error creating database: ".$conn->error;
-    }
+    
+    // prepare and bind
+    $stmt = $conn->prepare("INSERT INTO DataUser (FirstName, LastName, Age, Hometown, Job) VALUES(?,?,?,?,?)");
+    $stmt->bind_param("ssiss",$FirstName, $LastName, $Age,$Hometown, $Job);
+    // set parameters and execute
+    $FirstName ="Tran";
+    $LastName = "Sinh";
+    $Age = 21;
+    $Hometown ="Phu Yen";
+    $Job = "Dev";
+    $stmt->execute();
+
+    $FirstName ="Nguyen";
+    $LastName = "Tien";
+    $Age = 23;
+    $Hometown ="Ho Chi Minh";
+    $Job = "Dien tu";
+    $stmt->execute();
+
+    $FirstName ="Vo";
+    $LastName = "Phuc";
+    $Age = 24;
+    $Hometown ="Da Nang";
+    $Job = "Quan tri";
+    $stmt->execute();
+
+    $FirstName ="Nguyen";
+    $LastName = "Hai";
+    $Age = 23;
+    $Hometown ="Song Cau";
+    $Job = "Leader";
+    $stmt->execute();
+
+    echo "New records created successfully";
+    $stmt->close();
     $conn->close();
 ?>
